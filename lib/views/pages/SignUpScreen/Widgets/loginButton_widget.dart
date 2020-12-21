ae73a246fbf3e092066e8e2a4866143cac1f79c9
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-12-21 19:16:22 +0530

    Login button widget added

---
+++ lib/views/pages/SignUpScreen/Widgets/loginButton_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/LoginScreen/login_screen.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/SignUpScreen/signUp_screen.dart';
+import 'package:get/get.dart';
+
+Widget loginButtonSignUpScreen() {
+  return GestureDetector(
+    onTap: () {
+      Get.off(LoginScreen());
+    },
+    child: Padding(
+      padding: const EdgeInsets.only(bottom: 58.0, top: 25),
+      child: Container(
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.yellow[400],
+              Colors.yellow,
+              Colors.yellow[600],
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 1.6,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
