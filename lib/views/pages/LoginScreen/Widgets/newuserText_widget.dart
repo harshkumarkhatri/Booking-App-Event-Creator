f5089a5e7ce41f0526112ffb9c0b5c232e23d352
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-12-24 21:12:51 +0530

    New user text widget for login screen added

---
+++ lib/views/pages/LoginScreen/Widgets/newuserText_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Widget newUserText() {
+  return Container(
+    child: Center(
+      child: Text(
+        "New User?",
+        style: TextStyle(
+          color: Colors.yellow,
+        ),
+      ),
+    ),
+  );
+}
