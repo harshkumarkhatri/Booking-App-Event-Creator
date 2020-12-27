d5fc81ca4f676e9b316a2bdb20871bc8b71a98f0
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-12-27 19:45:46 +0530

    Added widget for about the user tile

---
+++ lib/views/pages/MainScreen/Widgets/aboutTheUserTile_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget aboutTheUserTile() {
+  return ListTile(
+    title: Text('About the User'),
+    onTap: () {
+      // Update the state of the app.
+      // ...
+    },
+  );
+}
