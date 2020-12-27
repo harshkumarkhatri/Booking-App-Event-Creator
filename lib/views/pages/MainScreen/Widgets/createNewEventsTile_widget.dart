25013246bdafa28ead2db1f43abcff6eae0487f5
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-12-27 19:46:08 +0530

    Added widget for create new event tile

---
+++ lib/views/pages/MainScreen/Widgets/createNewEventsTile_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget createNewEventTile() {
+  return ListTile(
+    title: Text('Create New Events'),
+    onTap: () {
+      // Update the state of the app.
+      // ...
+    },
+  );
+}
