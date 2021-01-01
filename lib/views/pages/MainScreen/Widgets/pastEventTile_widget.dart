3356568c8a5c983bfd406c1adb8eb64d3a714cb3
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-01 20:59:10 +0530

    Added widget for past event tile

---
+++ lib/views/pages/MainScreen/Widgets/pastEventTile_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget pastEventTile() {
+  return ListTile(
+    title: Text('Past Events'),
+    onTap: () {
+      // Update the state of the app.
+      // ...
+    },
+  );
+}
