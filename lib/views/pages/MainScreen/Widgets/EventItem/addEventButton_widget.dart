b94e031a4f287109ad5d9b1ed4435e0a65e8f9fb
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-05 14:13:13 +0530

    Added event button widget for main screen

---
+++ lib/views/pages/MainScreen/Widgets/EventItem/addEventButton_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/AddEventsScreen/addEvents_screen.dart';
+import 'package:get/get.dart';
+
+Widget addEventButton() {
+  return RaisedButton(
+    onPressed: () {
+      Get.to(AddEvents());
+    },
+    color: Colors.black,
+    textColor: Colors.orange,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(
+        14,
+      ),
+    ),
+    elevation: 12,
+    child: Container(
+      child: Text(
+        "Add event".toUpperCase(),
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1.5,
+        ),
+      ),
+    ),
+  );
+}
