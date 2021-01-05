7bef1eccae047dba66b2014a6f8089be1b597558
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-05 14:12:33 +0530

    Initialized settings screen

---
+++ lib/views/pages/SettingsScreen/settingScreen_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  @override
+  _SettingScreenState createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
