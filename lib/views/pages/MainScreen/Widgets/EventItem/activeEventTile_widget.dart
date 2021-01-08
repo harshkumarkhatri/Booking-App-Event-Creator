bd3832cbe42b7206c39d3408871f0016270cb63a
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-08 22:13:37 +0530

    Active event Tile added for main screen drawer

---
+++ lib/views/pages/MainScreen/Widgets/EventItem/activeEventTile_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/ActiveEvents/activeEvents_screen.dart';
+import 'package:get/get.dart';
+
+Widget activeEventTile() {
+  return ListTile(
+    tileColor: Colors.black,
+    title: Text(
+      'Active Events',
+      style: TextStyle(
+        color: Colors.orange,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    onTap: () {
+      // Update the state of the app.
+      // ...
+      Get.back();
+      Get.to(ActiveEventsScreen());
+    },
+  );
+}
