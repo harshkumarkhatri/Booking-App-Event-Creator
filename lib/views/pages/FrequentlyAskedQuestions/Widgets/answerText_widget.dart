8afea33616db22e1e55d7204e849f6fb493e70bc
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-15 13:54:09 +0530

    Added answers widget for frequently asked questions

---
+++ lib/views/pages/FrequentlyAskedQuestions/Widgets/answerText_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Widget answerText_widget(String text) {
+  return new AnimatedContainer(
+    decoration: BoxDecoration(
+      color: Colors.black,
+    ),
+    duration: const Duration(milliseconds: 120),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(
+        text,
+        style: TextStyle(
+          color: Colors.orange,
+          fontSize: 16,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.6,
+          height: 1.1,
+        ),
+      ),
+    ),
+  );
+}
