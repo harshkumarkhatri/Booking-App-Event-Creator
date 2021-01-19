6f852a96043e006da63d653e987b6539af6df541
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-19 23:07:39 +0530

    Completed the event deatils screen inside active events

---
+++ lib/views/pages/ActiveEvents/EventDetails_screen/eventDetails_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/ActiveEvents/EventDetails_screen/Widgets/dividerEventDetail_widget.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/ActiveEvents/EventDetails_screen/Widgets/editEventText_widget.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/ActiveEvents/EventDetails_screen/Widgets/itemTextEventDetails_widgets.dart';
+
+class EventDetailsScreen extends StatefulWidget {
+  EventDetailsScreen({this.data});
+  Map<String, dynamic> data;
+
+  @override
+  _EventDetailsScreenState createState() => _EventDetailsScreenState();
+}
+
+class _EventDetailsScreenState extends State<EventDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.orange,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: Text(
+          widget.data["eventName"].toString().toUpperCase(),
+          style: TextStyle(
+            color: Colors.orange,
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.orange,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 15,
+            right: 15.0,
+            top: 8,
+          ),
+          child: Column(
+            children: [
+              itemTextEventDetails_widget(
+                "User Name",
+                widget.data['fullUserName'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Contact Email",
+                widget.data['contactEmail'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Phone Number",
+                widget.data['phoneNumber'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Location",
+                widget.data['eventState'] + ", " + widget.data['eventCity'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Event Type",
+                widget.data['eventType'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Expected Audience",
+                widget.data['expectedAudience'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Timing",
+                widget.data['time'],
+              ),
+              dividerEventDetail(),
+              itemTextEventDetails_widget(
+                "Date (yyyy-mm-dd)",
+                widget.data['date'],
+              ),
+              dividerEventDetail(),
+              editEventText_widget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
