3f60a37eafecd9c3fc01899607f11b27055ebdbf
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-25 21:27:07 +0530

    Updated item text event detais widget for event details screen insdie active events

--- lib/views/pages/ActiveEvents/EventDetails_screen/Widgets/itemTextEventDetails_widgets.dart
+++ lib/views/pages/ActiveEvents/EventDetails_screen/Widgets/itemTextEventDetails_widgets.dart
@@ -6,21 +6,27 @@ Widget itemTextEventDetails_widget(String item, String contactEmail) {
       Container(
         child: Text(
           item,
+          maxLines: 9,
           style: TextStyle(
             color: Colors.black,
             fontSize: 18,
             fontWeight: FontWeight.w500,
           ),
+          overflow: TextOverflow.ellipsis,
         ),
       ),
       Spacer(),
-      Container(
-        child: Text(
-          contactEmail,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
+      Flexible(
+        child: Container(
+          child: Text(
+            contactEmail,
+            maxLines: 9,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       )
