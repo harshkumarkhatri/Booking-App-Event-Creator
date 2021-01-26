9a49a6b84cad5514f67443ca67aaaa7c050dff50
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-26 22:32:40 +0530

    Added text for field for input decoration in add events screen

---
+++ lib/views/pages/AddEventsScreen/Widgets/textForField_inputDecoration.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+InputDecoration textFormField_inputDecoration(
+    String labelText, String helperText) {
+  return InputDecoration(
+    alignLabelWithHint: true,
+    focusColor: Colors.black,
+    enabledBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
+    ),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
+    ),
+    border: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.black),
+    ),
+    labelText: labelText,
+    labelStyle: TextStyle(color: Colors.black),
+    helperText: helperText,
+    errorStyle: TextStyle(color: Colors.black, fontSize: 16),
+  );
+}
