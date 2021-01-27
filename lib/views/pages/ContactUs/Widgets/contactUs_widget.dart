49f3e27cd37c366b3a512049bf23a95e4c8df3fd
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-01-27 23:01:46 +0530

    Added contact us widget for contact us screen

---
+++ lib/views/pages/ContactUs/Widgets/contactUs_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+Widget contactUs_widget(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 55,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(12),
+      gradient: LinearGradient(
+        begin: Alignment.bottomCenter,
+        end: Alignment.topCenter,
+        colors: [
+          Colors.black.withOpacity(0.6),
+          Colors.black.withOpacity(0.7),
+          Colors.black.withOpacity(0.8),
+        ],
+      ),
+    ),
+    child: Center(
+      child: Text(
+        "Submit",
+        style: TextStyle(
+          fontSize: 32,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 1.6,
+          color: Colors.orange.withOpacity(0.6),
+        ),
+      ),
+    ),
+  );
+}
