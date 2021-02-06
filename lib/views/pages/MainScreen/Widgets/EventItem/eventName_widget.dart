3928d6892cc11c398b1d0e498cabaecb3c5068d1
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-02-06 23:47:19 +0530

    Added event name widget for event item in main screen

--- lib/views/pages/MainScreen/Widgets/EventItem/eventName_widget.dart
+++ lib/views/pages/MainScreen/Widgets/EventItem/eventName_widget.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-Widget eventName_widget(dynamic snapshot, int index) {
+Widget eventName_widget(dynamic snapshot, int index, String email) {
+  FieldPath field = FieldPath.fromString(email);
+
   return Container(
     child: Text(
-      snapshot.data["${index + 1}"]['eventName'].toString(),
+      snapshot.data["${index + 1}_${field.hashCode}"]['eventName'].toString(),
       style: TextStyle(
         color: Colors.orange,
         fontSize: 18,
