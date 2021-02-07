a320bd34634275012292ece0c1d736da00e0ecaa
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-02-07 21:06:54 +0530

    Added location and edit widget for event item in main screen

--- lib/views/pages/MainScreen/Widgets/EventItem/locationAndEdit_widget.dart
+++ lib/views/pages/MainScreen/Widgets/EventItem/locationAndEdit_widget.dart
@@ -1,22 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_booking_app_event_creator/views/pages/EditEvent/editEvent_screen.dart';
 import 'package:get/get.dart';
 
-Widget locationAndEdit_widget(dynamic snapshot, int index) {
+Widget locationAndEdit_widget(dynamic snapshot, int index, String email) {
+  FieldPath field = FieldPath.fromString(email);
+
   return Padding(
     padding: const EdgeInsets.only(top: 4.0),
     child: Row(
       children: [
         Icon(Icons.location_on, color: Colors.orange),
-        Container(
-          padding: EdgeInsets.only(left: 8),
-          child: Text(
-            snapshot.data['${index + 1}']['eventCity'].toString() +
-                ", " +
-                snapshot.data['${index + 1}']["eventState"].toString(),
-            style: TextStyle(
-              color: Colors.orange,
-              fontWeight: FontWeight.w600,
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.only(left: 8),
+            child: Text(
+              snapshot.data['${index + 1}_${field.hashCode}']['eventCity']
+                      .toString() +
+                  ", " +
+                  snapshot.data['${index + 1}_${field.hashCode}']["eventState"]
+                      .toString(),
+              style: TextStyle(
+                color: Colors.orange,
+                fontWeight: FontWeight.w600,
+              ),
+              overflow: TextOverflow.ellipsis,
             ),
           ),
         ),
