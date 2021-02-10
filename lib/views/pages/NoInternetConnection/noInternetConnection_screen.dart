5304b8dac62dd9b6c5ca31844eb22e0b5487c569
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-02-10 23:06:16 +0530

    Added no internet screen

---
+++ lib/views/pages/NoInternetConnection/noInternetConnection_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class NoInternetConnectionScreen extends StatefulWidget {
+  @override
+  _NoInternetConnectionScreenState createState() =>
+      _NoInternetConnectionScreenState();
+}
+
+class _NoInternetConnectionScreenState
+    extends State<NoInternetConnectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "No internet connection",
+          style: TextStyle(
+            color: Colors.orange,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.orange,
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.wifi,
+              color: Colors.black,
+              size: 80,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Text(
+                "No Connection",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Check your ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Wifi ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "or ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Mobile ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "connection.",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
