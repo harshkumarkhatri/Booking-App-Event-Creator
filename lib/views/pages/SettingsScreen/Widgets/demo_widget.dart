51248e1040f3df3484bbafc906b3966c1798a8db
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-02-12 22:39:33 +0530

    Added item text widget for adding items inside the settings screen

---
+++ lib/views/pages/SettingsScreen/Widgets/demo_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Widget demoWidget(IconData icon, String text, Widget page) {
+  return GestureDetector(
+    behavior: HitTestBehavior.opaque,
+    onTap: () {
+      Get.to(page);
+    },
+    child: Container(
+      // color: Colors.pink,
+      alignment: Alignment.centerLeft,
+      height: 45,
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Icon(icon),
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              // color: Colors.orange,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Spacer(),
+          Icon(
+            Icons.arrow_forward_ios,
+          ),
+        ],
+      ),
+    ),
+  );
+}
