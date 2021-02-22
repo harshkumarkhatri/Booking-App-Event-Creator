3d20e28a61a32ebdbc7dfbb261207cab041f3037
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-02-22 22:50:30 +0530

    Added details Text widget for event item in main screen

---
+++ lib/views/pages/MainScreen/Widgets/EventItem/detailsText_widget.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/RegisteredUsers/registeredUsers_screen.dart';
+import 'package:get/get.dart';
+
+Widget detailsText_widget(dynamic snapshot, int index, String email) {
+  FieldPath field = FieldPath.fromString(email);
+
+  return GestureDetector(
+    onTap: () {
+      Get.to(RegisteredUsers(snapshot, index, email));
+    },
+    child: Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Container(
+        child: Text(
+          "Registered Users",
+          style: TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    ),
+  );
+}
