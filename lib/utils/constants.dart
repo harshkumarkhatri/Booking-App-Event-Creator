aadc070ff1c1738beaa16ab8c7b2b131c0c6ecf4
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-03-08 20:50:56 +0530

    Added a constants file to store all the constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,52 @@
+// Holds all the values/variables which won't be changing throughout the app
+
+class Constants {
+  static List<String> stateList = <String>[
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    'Assam',
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujrat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+  ];
+
+  static List<String> eventTypes = [
+    "Exhibition",
+    "Info Session",
+    "Online Streaming",
+    "Workshop",
+    "E-Sports",
+    "BootCamp",
+    "Speaker Session",
+  ];
+
+  static const editYourUserNameText =
+      "You can edit your username through the\n\"Edit My Name\" option in Settings.";
+  static const versionText = "Version 1.0.0";
+  static const aboutTheAppText =
+      "Eventizer is an app that helps you to create or schedule your events on the go and let people know about the events which you are organizing. hrough this app you can let masses know about your event and increase the reach of your event. Every time a new event is created, users will be getting a notification/mail about it and then they can register for your event.";
+  static const linkAreNotEditableText =
+      "Event link cannot be changed later. Make sure to include 'http' or 'https' at the begining";
+}
