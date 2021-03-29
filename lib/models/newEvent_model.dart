4cfcc8c5acebe21400e12255a37d6955a75c3fff
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-03-29 19:55:36 +0530

    Add new event model and basic data model-Demo

---
+++ lib/models/newEvent_model.dart
@@ -0,0 +1,28 @@
+// Demo model for how the new event should be like.
+
+class NewEventModel {
+  final String contactEmail;
+  final String date;
+  final String eventCity;
+  final String eventState;
+  final String eventName;
+  final String eventStatus;
+  final String eventType;
+  final String expectedAudience;
+  final String fullUserName;
+  final String phoneNumber;
+  final String time;
+
+  NewEventModel(
+      this.contactEmail,
+      this.date,
+      this.eventCity,
+      this.eventState,
+      this.eventName,
+      this.eventStatus,
+      this.eventType,
+      this.expectedAudience,
+      this.fullUserName,
+      this.phoneNumber,
+      this.time);
+}
