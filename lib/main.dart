32f112d626c7942eca2106e57be8ce71311b1544
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-03-29 19:54:05 +0530

    Updated main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,62 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_booking_app_event_creator/repositories/subToTopic_fcm.dart';
+import 'package:flutter_booking_app_event_creator/responses/checkConnection.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/LoginSignUpScreen/loginSignup_screen.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/MainScreen/mainScreen_screen.dart';
+import 'package:flutter_booking_app_event_creator/views/pages/NoInternetConnection/noInternetConnection_screen.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final FlutterSecureStorage storage = FlutterSecureStorage();
+  String email = await storage.read(key: "email");
+  String uid = await storage.read(key: "uid");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp(email));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  
-
-  final String title;
+class MyApp extends StatefulWidget {
+  MyApp(String this.email);
+  String email;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  String connection = null;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    checkConn().then((value) {
+      setState(() {
+        connection = value;
+      });
     });
+    subToTopic_fcm();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return GetMaterialApp(
+        title: 'Eventizer',
+        theme: ThemeData(
+          canvasColor: Colors.black,
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
+        home: connection == null
+            ? CircularProgressIndicator()
+            : connection == "none"
+                ? NoInternetConnectionScreen()
+                : widget.email == null
+                    ? LoginSignUpScreen()
+                    : MainScreen());
   }
 }
